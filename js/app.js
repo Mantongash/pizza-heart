3f91653b16a7fc85d3814a9a33d8ea0f1686f456
Mantongash <you@example.com>
2019-11-11 12:51:33 +0300

    Define variables for chicago pizza

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// Chicago Pizza Variables
+const order = document.getElementById("order");
+const
+
+order.addEventListener("click", function(event) {
+  event.preventDefault();
+  // Chicago Pizza Variables
+  const chicagoQuantity = parseInt(
+    document.getElementById("chicago-quantity").value
+  );
+  const chicagoSize = parseInt(document.getElementById("chicago-size").value);
+  const groundBeef = document.getElementById("beef");
+  const sausage = document.getElementById("sausage");
+  const pepperoni = document.getElementById("pepperoni");
+  const onions = document.getElementById("onions");
+  const mushrooms = document.getElementById("mushrooms");
+  const chicagoCrust = parseInt(document.getElementById("chicago-crust").value);
+
+  
+});
