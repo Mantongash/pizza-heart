6abf7f1e6fe7cee29045ddc8d4e4dff3240cc86d
Mantongash <you@example.com>
2019-11-12 04:18:42 +0300

    Added logic for the chicago pizza

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 // Chicago Pizza Variables
-const order = document.getElementById("order");
-const
+const order = document.getElementById("add-chicago-order");
 
 order.addEventListener("click", function(event) {
   event.preventDefault();
@@ -8,13 +7,43 @@ order.addEventListener("click", function(event) {
   const chicagoQuantity = parseInt(
     document.getElementById("chicago-quantity").value
   );
-  const chicagoSize = parseInt(document.getElementById("chicago-size").value);
+  const chicagoSize = document.getElementById("chicago-size");
   const groundBeef = document.getElementById("beef");
   const sausage = document.getElementById("sausage");
   const pepperoni = document.getElementById("pepperoni");
   const onions = document.getElementById("onions");
   const mushrooms = document.getElementById("mushrooms");
-  const chicagoCrust = parseInt(document.getElementById("chicago-crust").value);
+  const chicagoCrust = document.getElementById("chicago-crust");
+  const toppings = [groundBeef, sausage, pepperoni, onions, mushrooms];
 
-  
+  const toppingsTotal = function() {
+    let totals = 0;
+    toppings.forEach(function(topping, index) {
+      if (topping.checked) {
+        return (totals += parseInt(topping.value));
+      }
+    });
+    return totals;
+  };
+
+  const results = function() {
+    const tbl = document.getElementById("tbl");
+    const row = tbl.insertRow();
+    const cell1 = row.insertCell();
+    const cell2 = row.insertCell();
+    const cell3 = row.insertCell();
+    const cell4 = row.insertCell();
+    const cell5 = row.insertCell();
+    cell1.innerHTML = chicagoQuantity;
+    cell2.innerHTML = chicagoSize.options[chicagoSize.selectedIndex].text;
+    cell3.innerHTML = toppingsTotal();
+    cell4.innerHTML = chicagoCrust.options[chicagoCrust.selectedIndex].text;
+    cell5.innerHTML =
+      chicagoQuantity *
+      (parseInt(chicagoSize.value) +
+        toppingsTotal() +
+        parseInt(chicagoCrust.value));
+  };
+
+  results();
 });
